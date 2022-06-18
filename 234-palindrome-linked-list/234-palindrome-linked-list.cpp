3980822f55d30952023b3db156694005a4a197f7
Md. Muzahidul Islam <48602232+Muzahid037@users.noreply.github.com>
2022-06-19 01:57:26 +0600

    Time: 241 ms (91.50%), Space: 114.1 MB (72.83%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -1,56 +1,45 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *  int val;
- *  ListNode * next;
- *  ListNode() : val(0), next(nullptr) {}
- *  ListNode(int x) : val(x), next(nullptr) {}
- *  ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
-class Solution
-{
-    public:
-        bool isPalindrome(ListNode *head)
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *slow=head,*fast=head;
+        while(fast!=NULL && fast->next!=NULL)
         {
-            ListNode *fast = head, *slow = head;
-            while (fast != NULL && fast->next != NULL)
-            {
-                fast = fast->next->next;
-                slow = slow->next;
-            }
-            if (fast != NULL)
-            {
-                slow = slow->next;
-            }
-           	// cout<<slow->val<<endl;
-
-           	//reverse second part
-
-            ListNode *prev = NULL;
-            while (slow!=NULL)
-            {
-                ListNode *x = slow->next;
-                slow->next = prev;
-                if (x == NULL)
-                {
-                    break;
-                }
-                prev = slow;
-                slow = x;
-            }
-
-            while (slow != NULL)
-            {
-                cout << slow->val << " " << head->val << endl;
-               	if(slow->val!=head->val)
-               	{
-               	    return false;
-               	}
-                slow = slow->next;
-                head = head->next;
-            }
-
-            return true;
+            fast=fast->next->next;
+            slow=slow->next;
         }
+        if(fast!=NULL)
+        {
+            slow=slow->next;
+        }
+        ListNode *prev=NULL;
+        while(slow!=NULL)
+        {
+            ListNode *next=slow->next;
+            slow->next=prev;
+            prev=slow;
+            slow=next;
+        }
+        while(prev!=NULL)
+        {
+            if(prev->val!=head->val)
+            {
+                return false;
+            }
+            prev=prev->next;
+            head=head->next;
+        }
+            
+            
+     return true;
+    }
 };
