32257127f2a86d54a6109307c7f651d8a0b2d1ca
Md. Muzahidul Islam <48602232+Muzahid037@users.noreply.github.com>
2022-08-03 11:41:57 +0600

    Time: 28 ms (68.25%), Space: 5.9 MB (28.46%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         bool isP=false;
-        while(n%3==0 && n>1)
+        while(n>1 && n%3==0)
         {
             n/=3;
         }
